2a720e1091ded052280fe9a28bdb62fa53d5e962
Clara Keller <js.pwns@gmail.com>
2026-09-05 17:34:39 -0700

    add incus preseed

--- system/profiles/virtualization.nix
+++ system/profiles/virtualization.nix
@@ -8,6 +8,7 @@
 in {
   networking.firewall.trustedInterfaces = [
     "virbr0"
+    "incusbr0"
   ];
 
   boot.extraModprobeConfig = ''
@@ -22,6 +23,65 @@ in {
     incus = {
       enable = true;
       ui.enable = true;
+
+      preseed = {
+        config = {
+          "core.https_address" = "[::]:8443";
+        };
+
+        networks = [
+          {
+            config = {
+              "ipv4.address" = "auto";
+              "ipv6.address" = "auto";
+            };
+            description = "";
+            name = "incusbr0";
+            type = "";
+            project = "default";
+          }
+        ];
+
+        storage_pools = [
+          {
+            config = {
+              size = "30GiB";
+            };
+            description = "";
+            name = "default";
+            driver = "btrfs";
+          }
+        ];
+
+        storage_volumes = [];
+
+        profiles = [
+          {
+            config = {};
+            description = "";
+            devices = {
+              eth0 = {
+                name = "eth0";
+                network = "incusbr0";
+                type = "nic";
+              };
+
+              root = {
+                path = "/";
+                pool = "default";
+                type = "disk";
+              };
+            };
+            name = "default";
+            project = "default";
+          }
+        ];
+
+        projects = [];
+        certificates = [];
+        cluster_groups = [];
+        cluster = null;
+      };
     };
 
     docker = {
